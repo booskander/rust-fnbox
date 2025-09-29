6da5f8b70a41eea48529d9ce4f257e767a7ad30c
Booskander <skander.hannachi@polymtl.ca>
2025-09-29 12:07:57 -0400

    system module

---
+++ rsm/src/system.rs
@@ -0,0 +1,50 @@
+use std::collections::BTreeMap;
+
+pub struct Pallet {
+    block_number: u32,
+    nonce: BTreeMap<String, u32>,
+}
+
+impl Pallet {
+    pub fn new() -> Self {
+        Self {
+            block_number: 0,
+            nonce: BTreeMap::new(),
+        }
+    }
+    pub fn block_number(&self) -> u32 {
+        self.block_number
+    }
+
+    pub fn inc_block_number(&mut self) {
+        self.block_number = self.block_number.checked_add(1).unwrap();
+    }
+
+    pub fn inc_nonce(&mut self, who: &String) {
+        let nonce = self.nonce.get(who).unwrap_or(&0);
+        self.nonce.insert(who.clone(), nonce + 1);
+    }
+
+    pub fn get_nonce(&mut self, who: &String) -> u32 {
+        *self.nonce.get(who).unwrap_or(&0)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::system::Pallet;
+    #[test]
+    fn init_system() {
+        let system: Pallet = super::Pallet::new();
+        assert_eq!(system.block_number(), 0);
+    }
+
+    #[test]
+    fn inc_nonce() {
+        let alice: String = "Alice".to_string();
+        let mut system: Pallet = super::Pallet::new();
+
+        system.inc_nonce(&alice);
+        assert_eq!(system.get_nonce(&alice), 1);
+    }
+}
